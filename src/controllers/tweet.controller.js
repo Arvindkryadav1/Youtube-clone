46b854fd41c2eaa45e12f54b7339f2b2d5f71efa
Arvind Kumar <aarviiyadav19@gmail.com>
2024-02-13 20:20:46 +0530

    add tweet controllers

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -9,20 +9,96 @@ const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
     const {userId} = req.params
     if(!userId){
-        throw new ApiEr
+        throw new ApiError(400, "User Id not found")
     }
+    if(!isValidObjectId(userId)){
+        throw new ApiError(400, "User Id is not valid")
+    }
+
+    const tweet = await Tweet.create({
+        content,
+        owner: req.user._id
+    })
+
+    const postedTweet = await Tweet.findById(tweet._id)
+    if(!postedTweet){
+        throw new ApiError(500, "Something went wrong while posting the tweet")
+    }
+
+    return res
+             .status(200)
+             .json(200, postedTweet, "Tweet posted successfully")
+
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+    const {userId} = req.params
+    if(!userId){
+        throw new ApiError(400, "No user id found")
+    }
+    if(isValidObjectId(userId)){
+        throw new ApiError(400, "User id is invalid")
+    }
+    const tweets = Tweet.find({
+        owner: userId
+    })
+    if(!tweets){
+        throw new ApiError(400, "something went wrong while getting tweets")
+    }
+    return res.status(200).json(200, tweets, "Tweets fetched successfully")
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const {tweetId} = req.params
+    if(!tweetId?.trim()){
+        throw new ApiError(400, "Tweet id is required")
+    }
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Tweet id is invalid")
+    }
+    const {content} = req.body
+    if(!content){
+        throw new ApiError(400, "Content is required")
+    }
+    await Tweet.findOne({_id:tweetId})
+    const updatedTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set: {
+                content
+            },
+        },
+        {
+            new: true
+        }
+    )
+    if(!updatedTweet){
+        throw new ApiError(500, "Something went wrong while updating the tweet")
+    }
+    return res.status(200).json(200, updatedTweet, "Tweet updated successfully")
+
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
     //TODO: delete tweet
+    const {tweetId} = req.params
+    if(!tweetId){
+        throw new ApiError(400, "Tweet Id is required")
+    }
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Tweet id is invalid")
+    }
+
+    const deletedTweet = await Tweet.findByIdAndDelete(tweetId)
+
+    if(!deletedTweet){
+     throw new ApiError(500, "Something went wrong while deleting the tweet")
+    }
+    return res
+            .status(200)
+            .json(200, {}, "Tweet deleted successfully")
 })
 
 export {
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -4,27 +4,153 @@ import {User} from "../models/user.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
-import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import {uploadOnCloudinary, deleteFromCloudinary, deleteVideo} from "../utils/cloudinary.js"
+import { extractPublicId } from "cloudinary-build-url"
 
 
 const getAllVideos = asyncHandler(async (req, res) => {
     const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
     //TODO: get all videos based on query, sort, pagination
+    let videoQuery = {}
+
+    if(query){
+        videoQuery = {...videoQuery, title: new RegExp(query, "i")}
+    }
+    if(userId){
+        if(!isValidObjectId(userId)){
+            throw new ApiError(400, "Invalid user id")
+        }
+
+        videoQuery = {...videoQuery, owner: userId}
+    }
+
+    const totalVideos = await Video.countDocuments(videoQuery)
+    let sortCriteria = {}
+    if(sortBy){
+        sortCriteria[sortBy] = sortType === "desc" ? -1 :1
+    }
+    const videos = await Video.find(videoQuery)
+                              .sort(sortCriteria)
+                              .skip((page-1)*limit)
+                              .limit(Number(limit))
+    return res
+            .status(200)
+            .json(200, videos, "Videos fetched successfully")
 })
 
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    if(!title || !description){
+        throw new ApiError(400, "All fields are required")
+    }
+    // Uploading files on local server
+    const videoLocalPath = req.files?.videoFile[0]?.path
+    if(!videoLocalPath){
+        throw new ApiError(400, "Video localpath is required")
+    }
+
+    const thumbnailLocalPath = req.files?.thumbnail[0]?.path
+    if(!thumbnailLocalPath){
+        throw new ApiError(400, "Thumbnail local path is required")
+    }
+
+    //uploading files on cloudinary
+    const videoFile = await uploadOnCloudinary(videoLocalPath)
+    if(!videoFile){
+        throw new ApiError(500, "Something went wrong while uploading video on cloudinary")
+    }
+
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+    if(!thumbnail){
+        throw new ApiError(500, "Something went wrong while uploading thumbanil on cloudinary")
+    }
+
+    //saving video in DB
+    const video = await Video.create({
+        videoFile: videoFile?.url,
+        thumbnail: thumbnail?.url,
+        title,
+        description,
+        duration: videoFile?.duration,
+        owner: req.user._id
+    })
+
+    const uploadedVideo = await Video.findById(video._id)
+    if(!uploadedVideo){
+        throw new ApiError(500, "Something went while uploading video")
+    }
+    return res
+    .status(201)
+    .json(new ApiResponse(201, uploadedVideo, "Video Uploaded Successfully"));
 })
 
 const getVideoById = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: get video by id
+    if(!videoId){
+        throw new ApiError(400, "Video id is required")
+    }
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Video id is invalid")
+    }
+    const video = await Video.findById(videoId)
+    if(!video){
+        throw new ApiError(500, "Something went wrong while finding the video")
+    }
+
+    const user = await User.findById(req.user?._id)
+    user.watchHistory.push(videoId)
+    
+    await user.save({
+        validateBeforeSave: fasle
+    })
+    // increment of views count
+
+    video.views =+ 1
+    const view = await video.save({validateBeforeSave: false})
+    if(!view){
+        throw new ApiError(500, "Something went wrong while increasing the views")
+    }
+
+    return res.status(200).json(200, video, "Video fetched successfully")
 })
 
 const updateVideo = asyncHandler(async (req, res) => {
     const { videoId } = req.params
     //TODO: update video details like title, description, thumbnail
+    if(!videoId?.trim()){
+        throw new ApiError(400, "Video id is missing")
+    }
+    if(!isValidObjectId(videoId)){
+        throw new ApiError(400, "Video Id is invalid")
+    }
+
+    const {title, description} = req.body
+    if(!title || !description){
+        throw new ApiError(400, "All fields are required")
+    }
+
+    const thumbnailLocalPath = req.file?.path
+    if(!thumbnailLocalPath){
+        throw new ApiError(400, "Thumbnail local path is missing")
+    }
+    
+    const thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+    if(!thumbnail.url){
+        throw new ApiError(500, "Something went wrong while uploading on cloudinary")
+    }
+
+    //Deleting old
+
+    const oldVideo = await Video.findById(videoId)
+    const oldThumbnailURL = oldVideo.thumbnail
+    if(!oldThumbnailURL){
+        throw new ApiError(500, "Something went wrong while with thumbnail")
+    }
+
+    const thumbnailPublicId = extractPublicId(oldThumbnailURL)
+    const deletedOldThumbnail = await deleteFromCl
 
 })
 
